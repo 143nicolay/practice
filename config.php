b1fd93827b919c6ced5bd21bfa803bbf9d689297
Nicole Espiritu <146156901+143nicolay@users.noreply.github.com>
2024-11-20 12:14:19 +0800

    config.php

--- config.php
+++ config.php
@@ -7,10 +7,9 @@ define('DB_PASSWORD', "1234");
 define('DB_NAME', "demo");
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+$conn = new mysqli("DB_SERVERNAME", "DB_USERNAME", "DB_PASSWORD", "DB_NAME");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
